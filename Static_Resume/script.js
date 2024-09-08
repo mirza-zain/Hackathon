df70bcd6bd4eaf5295bb04a967cc84409ef5d3d2
mirza-zain <mirzazain269@gmail.com>
2024-09-08 08:54:12 +0500

    Milestone 01 completed

---
+++ Static_Resume/script.js
@@ -0,0 +1,21 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const show = document.getElementById('show');
+    const close = document.getElementById('hide');
+    const showskills = document.getElementById('showskills');
+    if (show && close && showskills) {
+        show.addEventListener('click', () => {
+            showskills.style.display = 'block';
+            show.style.opacity = '50%';
+            close.style.opacity = '100%';
+        });
+        close.addEventListener('click', () => {
+            showskills.style.display = 'none';
+            close.style.opacity = '50%';
+            show.style.opacity = '100%';
+        });
+    }
+    else {
+        console.error('One or more elements not found');
+    }
+});
